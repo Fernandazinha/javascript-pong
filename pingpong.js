adc65ca1d60af88f7b7b072e0c95ff5753f474b8
Fernandazinha <fernanda.rezende.silva16@escola.pr.gov.br>
2024-06-27 17:42:04 -0300

    cria bolinha 27/06/2024

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,8 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas (600,400);
@@ -6,4 +11,12 @@ function setup(){
 function draw(){
     //aqui vou por a cor d "mesa"
     background(244,164,96);
+    //chamado a função
+    criaBolinha(xBolinha, yBolinha, tamBolinha)
 }
+
+
+//função bolinha
+function criaBolinha(Bolinha, yBolinha, tamBolinha)[
+    circle (xBolinha,yBolinha,tamBolinha);
+]
